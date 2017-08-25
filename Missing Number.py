80966d95d36cafee093bc9367150ad775a2f48d9
anderson <anderson.t.peng@gmail.com>
2017-08-25 00:14:22 -0500

    use XOR but it may have a better solution

--- Missing Number.py
+++ Missing Number.py
@@ -9,4 +9,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        for i 
+        # if nums == [0]:
+        #     return 1
+        # List = list(range(len(nums)+1))
+        # for i in range(len(List)):
+        #     if i in nums:
+        #         del List[i]
+        #     else:
+        #         return i
+        x = list(range(len(nums)+1))
+        y = nums
+        x.extend(y)
+        res = 0
+        for num in x:
+            res ^= num
+        return res
+
+
+nums = [0,1,2,4]
+# nums = [0]
+object = Solution()
+print(object.missingNumber(nums))
