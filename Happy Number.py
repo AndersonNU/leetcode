6995264b626f50e4690056090f4f4cd6b52dd2d9
anderson <anderson.t.peng@gmail.com>
2017-08-30 22:35:12 -0500

    commit

---
+++ Happy Number.py
@@ -0,0 +1,39 @@
+# Write an algorithm to determine if a number is "happy".
+#
+# A happy number is a number defined by the following process: Starting with any positive integer, replace the number
+# by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it
+# loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+#
+# Example: 19 is a happy number
+#
+# 1^2 + 9^2 = 82
+# 8^2 + 2^2 = 68
+# 6^2 + 8^2 = 100
+# 1^2 + 0^2 + 0^2 = 1
+
+
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        old_list = n
+        run = 0
+        while run < 10000:
+            new_list = [int(d) for d in str(old_list)]
+            result = 0
+            for element in new_list:
+                result += element * element
+            if result == 1:
+                return True
+            elif result ==4:
+                return False
+            else:
+                old_list = result
+            run += 1
+            print(result)
+
+n = 125
+object = Solution()
+print(object.isHappy(n))
