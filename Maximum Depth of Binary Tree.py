f375f3657aadb63ab002f7e381e81a37f254c7e7
Anderson <taoruipeng2017@u.northwestern.edu>
2017-09-06 15:54:08 -0500

    recursively solve binary tree problem

--- Maximum Depth of Binary Tree.py
+++ Maximum Depth of Binary Tree.py
@@ -15,8 +15,10 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+        if not root:
+            return 0
 
-
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
 
 object = Solution()
