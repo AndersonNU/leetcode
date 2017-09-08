404754c6bb44032f082481e1fc61d58216139a3b
Anderson <taoruipeng2017@u.northwestern.edu>
2017-09-08 14:19:30 -0500

    new intermediate level stuff

--- Counting Bits.py
+++ Counting Bits.py
@@ -24,9 +24,9 @@ class Solution(object):
         # for i in range(num+1):
         iniArr = [0]
         if num > 0:
-            amountToAdd = 1
             while len(iniArr) < num + 1:
                 iniArr.extend([x+1 for x in iniArr])
+                # print(iniArr)
 
         return iniArr[0:num+1]
 
