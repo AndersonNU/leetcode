7bc700e0ebb93b0ed48fd652099feb3a56b1bc64
anderson <anderson.t.peng@gmail.com>
2017-09-11 14:00:00 +0100

    testing

--- Array Nesting.py
+++ Array Nesting.py
@@ -29,3 +29,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        ans, step, n = 0, 0, len(nums)
+        seen = [False] * n
+        for i in range(n):
+            while not seen[i]:
+                seen[i] = True
+                i, step = nums[i], step + 1
+            ans = max(ans, step)
+            step = 0
+        return ans
+
+
+nums = [5,4,0,3,1,6,2]
+object = Solution()
+print(object.arrayNesting(nums))
