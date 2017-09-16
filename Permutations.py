524253e0bff1f6978e6cf652ce9d00159291c860
anderson <anderson.t.peng@gmail.com>
2017-09-15 23:55:02 -0500

    needs more

--- Permutations.py
+++ Permutations.py
@@ -19,3 +19,13 @@ class Solution(object):
     :type nums: List[int]
     :rtype: List[List[int]]
     """
+
+
+      perms = [[]]
+      for n in nums:
+        new_perms = []
+        for perm in perms:
+          for i in xrange(len(perm) + 1):
+            new_perms.append(perm[:i] + [n] + perm[i:])  ###insert n
+        perms = new_perms
+      return perms        
