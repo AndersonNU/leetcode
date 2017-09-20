fb947129cc14c7e0b723d74a10a543df3337fafc
anderson <anderson.t.peng@gmail.com>
2017-09-19 22:10:21 -0500

    if I can always import things, the world will be a better place

--- Permutations.py
+++ Permutations.py
@@ -14,17 +14,24 @@
 
 
 class Solution(object):
-  def permute(self, nums):
-    """
-    :type nums: List[int]
-    :rtype: List[List[int]]
-    """
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        # perms = [[]]
+        # for n in nums:
+        #     new_perms = []
+        #     for perm in perms:
+        #         for i in range(len(perm) + 1):
+        #             new_perms.append(perm[:i] + [n] + perm[i:])  ###insert n
+        #             # print('new_perms is:', new_perms)
+        #     perms = new_perms
+        # return perms
+        from itertools import permutations
+        l = list(permutations(nums))
 
-      perms = [[]]
-      for n in nums:
-        new_perms = []
-        for perm in perms:
-          for i in xrange(len(perm) + 1):
-            new_perms.append(perm[:i] + [n] + perm[i:])  ###insert n
-        perms = new_perms
-      return perms
+        return l
+nums = [1,2,3,4,5,6,8,0]
+x = Solution()
+print(x.permute(nums))
