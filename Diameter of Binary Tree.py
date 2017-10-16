bf0a1a49428ba66f1808797cdcc3a42add216ad5
anderson <anderson.t.peng@gmail.com>
2017-10-15 23:48:54 -0500

    diameter of the tree node

--- Diameter of Binary Tree.py
+++ Diameter of Binary Tree.py
@@ -27,3 +27,14 @@ class Solution(object):
         """
         # The sum of the longest on the left side + the longest on the right side
 
+        self.best = 1
+
+        def depth(root):
+            if not root: return 0
+            ansL = depth(root.left)
+            ansR = depth(root.right)
+            self.best = max(self.best, ansL + ansR + 1)
+            return 1 + max(ansL, ansR)
+
+        depth(root)
+        return self.best - 1
